fc1fe18b836866bd75bf2e22f491172c62ef7f67
alessia-pasquero07 <a.pasquero.3232@vallauri.edu>
2026-09-16 12:40:11 +0200

    inizializzazione

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Student } from './student/student';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [Student],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('angular-students');
+  author = 'Alessia Pasquero';
 }
---
+++ src/app/student/student.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-student',
+  styleUrl: './student.css',
+  templateUrl: './student.html',
+})
+export class Student {
+  students:any[]=[{"name":"Pippo","city":"Topolandia","gender":"M","Present":true},
+    {"name":"Pippo","city":"Topolandia","gender":"M","Present":false},
+    {"name":"Pluto","city":"Topolandia","gender":"M","Present":true},
+    {"name":"Paperina","city":"Paperopoli","gender":"F","Present":false},
+    {"name":"Gastone","city":"Paperopoli","gender":"M","Present":true},
+    {"name":"Minnie","city":"Topolandia","gender":"F","Present":true},
+  ];//potrebbe anche non avere le virgolette sulle chiavi
+}
