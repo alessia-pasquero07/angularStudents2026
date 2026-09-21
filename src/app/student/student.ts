917d254a2756c60cb89526b033bec3b25a46140d
alessia-pasquero07 <a.pasquero.3232@vallauri.edu>
2026-09-21 10:35:09 +0200

    errori vari

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -8,9 +8,11 @@ import { NgClass, NgStyle } from '@angular/common';
   styleUrl: './student.css',
   templateUrl: './student.html',
 })
+const M_COLOR="lightblue";
+const  F_COLOR="pink";
+
 export class Student {
-  M_COLOR="lightblue"
-  F_COLOR="pink"
+ 
   students:any[]=[{"name":"Pippo","city":"Topolandia","gender":"M","Present":true},
     {"name":"Pippo","city":"Topolandia","gender":"M","Present":false},
     {"name":"Pluto","city":"Topolandia","gender":"M","Present":true},
@@ -18,4 +20,10 @@ export class Student {
     {"name":"Gastone","city":"Paperopoli","gender":"M","Present":true},
     {"name":"Minnie","city":"Topolandia","gender":"F","Present":true},
   ];//potrebbe anche non avere le virgolette sulle chiavi
+  
+    getSyle(s){
+        return {
+            'background-color': s.gender == 'F' ?  F_COLOR: M_COLOR
+        }
+    }
 }
